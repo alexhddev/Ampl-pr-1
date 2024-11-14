e1dd0480582f0ede3c2eae7b76f985583838ae30
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-11-14 16:04:17 +0200

    add amplify stiles and config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import { Authenticator } from '@aws-amplify/ui-react'
+import '@aws-amplify/ui-react/styles.css'
+import outputs from '../amplify_outputs.json';
+import { Amplify } from 'aws-amplify';
 
+Amplify.configure(outputs);
 function App() {
 
   return (
