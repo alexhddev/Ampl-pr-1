cc98839fcbb7267200585493ffb412016fee7d95
alexhddev <21276888+alexhddev@users.noreply.github.com>
2024-11-19 15:26:25 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,27 @@ import { Authenticator } from '@aws-amplify/ui-react'
 import '@aws-amplify/ui-react/styles.css'
 import outputs from '../amplify_outputs.json';
 import { Amplify } from 'aws-amplify';
+import TodoList from './TodoList';
+import { Hub } from 'aws-amplify/utils';
 
 Amplify.configure(outputs);
+Hub.listen('auth', (data) => {
+  console.log(data)
+});
 function App() {
 
   return (
     <>
       <h2>Hello</h2>
-      <Authenticator />
+      <TodoList />
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Hello {user?.username}</h1>
+            <button onClick={signOut}>Sign out</button>
+          </main>
+        )}
+      </Authenticator>
     </>
   )
 }
